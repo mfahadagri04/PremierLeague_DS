45b7330ed4385652c5cb5c803de1abb86ae6a35a
mfahadagri04 <126353489+mfahadagri04@users.noreply.github.com>
2024-09-25 22:23:16 -0600

    added data summarizations

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
 
+# Creating the dataframe
 df = pd.read_csv("matches.csv")
+
+# see the first 5 datas
+print(df.head())
+
+# See the last 5 datas
+print(df.tail())
+
+# Check the datasets information
+print(df.info())
+
+# Describe the dataset
+print(df.describe())
+print(df.describe().T)
+
+# Checking for empty datas
+print(df.isna().sum())
+print(df.isnull().sum())
